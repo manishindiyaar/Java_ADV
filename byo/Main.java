d06a466daf418763043603f1edf7c2a375f480d0
Manish <manishindiyaar@gmail.com>
2025-12-03 03:25:28 +0530

    added 2 new in byo

---
+++ byo/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main{
+    public static void main(String[] args){
+       int a= 10;
+       int b = 12;
+       int sum = a+b;
+       System.out.println("The sum of a and b is: " + sum);
+
+    }
+}
